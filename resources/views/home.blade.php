47e0533f1f1f81c13b7962d1ed4b75f573ad433e
himarisanto <161543405+himarisanto@users.noreply.github.com>
2024-03-04 15:20:43 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container" id="app">
+    <create-component v-if="routeIs('create')"></create-component>
+    <edit-component v-if="routeIs('edit')"></edit-component>
+    <data-component v-if="routeIs('data')"></data-component>
 </div>
 @endsection
+
+@push('scripts')
+<script>
+    window.onload = function () {
+        const app = new Vue({
+            el: '#app',
+            methods: {
+                routeIs(name) {
+                    return window.location.pathname.includes(name);
+                }
+            }
+        });
+    }
+</script>
+@endpush
